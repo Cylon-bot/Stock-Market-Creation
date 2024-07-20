c682de7a986a866c331e1262a682415f396646d6
Thibault delrieu <thibault.delrieu.pro@gmail.com>
2024-07-21 01:09:57 +0200

    add yaml conf

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,9 +1,14 @@
-pub mod yaml_connection;
 pub mod mathematical;
+pub mod yaml_connection;
 use serde::{Deserialize, Serialize};
 pub use yaml_connection::YamlFile;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct DatabaseGenerationConfiguration {
     name_database: String,
+    lower_time_frame: String,
+    probability_of_buying: f64,
+    probability_of_selling: f64,
+    probability_of_doing_nothing: f64,
+    number_of_player: u64,
 }
